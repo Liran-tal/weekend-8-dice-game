2bed6a0bb61abc2743e55c60bd8a1e796863ebde
Liran-tal <taliran44@gmail.com>
2021-12-18 16:45:28 +0200

    it worrrkkkkkksssssss

--- src/components/DiceGame/DiceGame.jsx
+++ src/components/DiceGame/DiceGame.jsx
@@ -5,14 +5,12 @@ import './styles/DiceGameMobile.css';
 import Player from "../Player/Player.component";
 import GameBoard from "../GameBoard/GameBoard";
 
-const NUM_OF_PLAYERS = 2;
-
 class DiceGame extends React.Component {
 	constructor () {
 		super();
 		this.state = {
 			pointsToWin: 100,
-			dices: [5, 5],
+			dices: [null, null],
 			buttonDisabled: false,
 			activePlayer: 'player1',
 			idlePlayer: 'player2',
@@ -21,8 +19,8 @@ class DiceGame extends React.Component {
 					id: 1,
 					playerName: 'player-1',
 					playerColor: '',
-					roundScore: 50,
-					gameScore: 10,
+					roundScore: 0,
+					gameScore: 0,
 					combinedScore: 0,
 					isActive: true,
 					isWinner: false
@@ -42,17 +40,20 @@ class DiceGame extends React.Component {
 	}
 	
 	render () {
+		console.log("Hello from Dice Game");
 		return (
 			<div className="">
-				Hello from Dice Game
 				<Player
 					data={this.state.players.player1}
-					/>
+				/>
 				<GameBoard
 					dices={this.state.dices}
 					pointsToWin={this.state.pointsToWin}
 					onClick={this.onClick}
-					/>
+				/>
+				<Player
+					data={this.state.players.player2}
+				/>
 			</div>
 		);
 	}
@@ -83,15 +84,17 @@ class DiceGame extends React.Component {
 				...prevState.players,
 				player1: {
 					...prevState.players.player1,
-					roundScore: 0,
 					gameScore: 0,
+					roundScore: 0,
+					combinedScore: 0,
 					isActive: true,
 					isWinner: false
 				},
-				player1: {
+				player2: {
 					...prevState.players.player2,
-					roundScore: 0,
 					gameScore: 0,
+					roundScore: 0,
+					combinedScore: 0,
 					isActive: false,
 					isWinner: false
 				}
@@ -108,13 +111,6 @@ class DiceGame extends React.Component {
 		const newRoundScore = this.state.players[activePlayer].roundScore + diceSum;
 		const gameScore = this.state.players[activePlayer].gameScore;
 
-		// ******************************************
-		console.log("dice1: ", dice1);
-		console.log("dice2: ", dice2);
-		console.log("diceSum: ", diceSum);
-		console.log("roundScore: ", this.state.players[activePlayer].roundScore);
-		console.log("newRoundScore: ", newRoundScore);
-// *********************************************
 		this.setState({dices: [dice1 - 1, dice2 - 1]});
 		
 		if ((diceSum) === 7) {
@@ -147,11 +143,6 @@ class DiceGame extends React.Component {
 		const roundScore = this.state.players[activePlayer].roundScore;
 		const gameScore = this.state.players[activePlayer].gameScore;
 		const total = roundScore + gameScore;
-		console.log("active player: ", activePlayer);
-		console.log(this.state.players[activePlayer]);
-		console.log("round score: ", roundScore);
-		console.log("game score: ", gameScore);
-		console.log("total score: ", total);
 
 		if (total >= this.state.pointsToWin) {
 			this.setState((prevState) => ({
@@ -182,11 +173,10 @@ class DiceGame extends React.Component {
 						isActive: false,
 					},
 					[prevState.idlePlayer]: {
-						...prevState[prevState.idlePlayer],
+						...prevState.players[prevState.idlePlayer],
 						isActive: true
 					}
 				}
-			
 			}))	
 		}
 	}
